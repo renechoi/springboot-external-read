cef327d4899bed7be8a64f066839f316782ec7b7
renechoi <renechoi90@gmail.com>
2023-03-15 13:54:48 +0900

    feat: data source constructor method

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -2,12 +2,14 @@ package hello;
 
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
+import hello.datasource.MyDataSourcePropertiesV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourcePropertiesV2.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
